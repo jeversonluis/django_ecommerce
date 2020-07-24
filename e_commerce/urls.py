040d117b1ac326beb0359c66ff240ec7ccf37114
Jeverson <jever.contato@gmail.com>
2020-07-24 12:23:51 -0300

    static_local/css/main.css - static_local/img/eCommerce.jpg - templates/home_page.html

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
 from .views import home_page, about_page, contact_page, login_page, register_page
@@ -25,3 +28,8 @@ urlpatterns = [
     path('login/', login_page),
     path('register/', register_page),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
