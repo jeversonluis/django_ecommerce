7d8efff6c2ad949453c3ac7e08a8fd0332ae4f3f
Jeverson <jever.contato@gmail.com>
2020-07-24 15:46:53 -0300

    update  products/templates/products/list.html - products/views.py

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -18,6 +18,8 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path
+
+from products.views import ProductListView
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -27,6 +29,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', login_page),
     path('register/', register_page),
+    path('products/', ProductListView.as_view()),
 ]
 
 if settings.DEBUG:
--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    template_name = "products/list.html"
+
+    """def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context """
+
+
+
+
+
